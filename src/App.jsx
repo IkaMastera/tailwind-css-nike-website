fda66bfd6f74a8a5465f5b6cabf31518d71dc42a
Irakli <112602982+IkaMastera@users.noreply.github.com>
2023-10-25 16:26:26 +0400

    add: nav bar component and data
    added nav bar component with styles and also some dummy data from index.js.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Hero, CustomerReviews, Footer, PopularProducts, 
-Services, SpecialOffer,Subscribe, SuperQuality} from './sections'
+Services, SpecialOffer,Subscribe, SuperQuality} from './sections';
+import Nav from './components/Nav';
 
 const App = () => (
   <main className="relative">
-     
+     <Nav />
      <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero />
      </section>
